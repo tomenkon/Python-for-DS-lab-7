d46219cc9aaa6a6864a05c96dd2bbb5e7ef443e7
Natalia Tomenko <nataliatomenko13@gmail.com>
2024-03-18 19:47:56 +0200

    added empty io functions

---
+++ app/io/input.py
@@ -0,0 +1,10 @@
+def input_from_console():
+    pass
+
+
+def input_from_file():
+    pass
+
+
+def input_with_pandas():
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,10 @@
+def output_to_console():
+    pass
+
+
+def output_to_file():
+    pass
+
+
+def output_with_pandas():
+    pass
